1dfbb72b773071ba666027eba72b853b70d08eda
Stuart Tottle <stuart.tottle@ldn-m-0013.home>
2019-11-03 11:51:27 +0000

    created AzureStorageModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ContainerListComponent } from './azure-storage/components/container-list.component';
-import { InputFileComponent } from './azure-storage/components/input-file.component';
-import { ItemsDeletedComponent } from './azure-storage/components/items-deleted.component';
-import { ItemsDownloadedComponent } from './azure-storage/components/items-downloaded.component';
-import { ItemsListComponent } from './azure-storage/components/items-list.component';
-import { ItemsUploadedComponent } from './azure-storage/components/items-uploaded.component';
-import { SelectedContainerComponent } from './azure-storage/components/selected-container.component';
-import {
-  azureBlobStorageFactory,
-  BLOB_STORAGE_TOKEN
-} from './azure-storage/services/token';
+import { AzureStorageModule } from './azure-storage/azure-storage.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ContainerListComponent,
-    ItemsListComponent,
-    SelectedContainerComponent,
-    InputFileComponent,
-    ItemsDownloadedComponent,
-    ItemsUploadedComponent,
-    ItemsDeletedComponent
-  ],
-  imports: [BrowserModule],
-  providers: [
-    {
-      provide: BLOB_STORAGE_TOKEN,
-      useFactory: azureBlobStorageFactory
-    }
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AzureStorageModule],
+
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/azure-storage/azure-storage.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ContainerListComponent } from './components/container-list.component';
+import { InputFileComponent } from './components/input-file.component';
+import { ItemsDeletedComponent } from './components/items-deleted.component';
+import { ItemsDownloadedComponent } from './components/items-downloaded.component';
+import { ItemsListComponent } from './components/items-list.component';
+import { ItemsUploadedComponent } from './components/items-uploaded.component';
+import { SelectedContainerComponent } from './components/selected-container.component';
+import { azureBlobStorageFactory, BLOB_STORAGE_TOKEN } from './services/token';
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [
+    ContainerListComponent,
+    ItemsListComponent,
+    SelectedContainerComponent,
+    InputFileComponent,
+    ItemsDownloadedComponent,
+    ItemsUploadedComponent,
+    ItemsDeletedComponent
+  ],
+  providers: [
+    {
+      provide: BLOB_STORAGE_TOKEN,
+      useFactory: azureBlobStorageFactory
+    }
+  ],
+  exports: [
+    ContainerListComponent,
+    ItemsListComponent,
+    SelectedContainerComponent,
+    InputFileComponent,
+    ItemsDownloadedComponent,
+    ItemsUploadedComponent,
+    ItemsDeletedComponent
+  ]
+})
+export class AzureStorageModule {}
