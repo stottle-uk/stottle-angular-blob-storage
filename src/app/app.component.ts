549022359f99288ebcf00a6802a8b6da40b68745
Stuart Tottle <stuart.tottle@ldn-m-0013.home>
2019-11-03 11:05:10 +0000

    created blob state service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,5 @@
 import { Component } from '@angular/core';
-import { DomSanitizer } from '@angular/platform-browser';
-import {
-  BlobDeleteResponse,
-  BlobDownloadResponseModel
-} from '@azure/storage-blob';
-import {
-  BehaviorSubject,
-  from,
-  MonoTypeOperatorFunction,
-  Observable,
-  OperatorFunction,
-  Subject
-} from 'rxjs';
-import {
-  filter,
-  finalize,
-  map,
-  mergeMap,
-  scan,
-  startWith,
-  switchMap,
-  withLatestFrom
-} from 'rxjs/operators';
-import { BlobStorageService } from './azure-storage/services/blob-storage.service';
-import { SasGeneratorService } from './azure-storage/services/sas-generator.service';
-import {
-  BlobContainerRequest,
-  BlobItem,
-  BlobItemDownload,
-  BlobItemUpload,
-  BlobStorageRequest,
-  Dictionary
-} from './azure-storage/types/azure-storage';
+import { BlobStateService } from './azure-storage/services/blob-state.service';
 
 @Component({
   selector: 'app-root',
@@ -75,220 +43,28 @@ import {
   styles: []
 })
 export class AppComponent {
-  selectedContainerInner$ = new BehaviorSubject<string>(undefined);
-  uploadQueueInner$ = new Subject<FileList>();
-  downloadQueueInner$ = new Subject<string>();
-  deleteQueueInner$ = new Subject<string>();
+  containers$ = this.blobState.containers$;
+  filesInContainer$ = this.blobState.filesInContainer$;
+  blobUploadProgress$ = this.blobState.blobUploadProgress$;
+  blobDownloadResponse$ = this.blobState.blobDownloadResponse$;
+  blobDeleteResponse$ = this.blobState.blobDeleteResponse$;
+  selectedContainer$ = this.blobState.selectedContainer$;
 
-  containers$ = this.getStorageOptions().pipe(
-    switchMap(options => this.blobStorage.getContainers(options))
-  );
-  filesInContainer$ = this.selectedContainer$.pipe(
-    filter(containerName => !!containerName),
-    withLatestFrom(this.getStorageOptions()),
-    switchMap(([containerName, options]) =>
-      this.blobStorage.listBlobsInContainer({
-        ...options,
-        containerName
-      })
-    )
-  );
-  blobUploadProgress$ = this.uploadQueue$.pipe(
-    mergeMap(file => this.uploadFile(file)),
-    this.scanEntries()
-  );
-  blobDownloadResponse$ = this.downloadQueue$.pipe(
-    mergeMap(filename => this.downloadFile(filename)),
-    this.scanEntries()
-  );
-  blobDeleteResponse$ = this.deleteQueue$.pipe(
-    mergeMap(filename => this.deleteFile(filename)),
-    this.scanEntries()
-  );
-
-  get selectedContainer$() {
-    return this.selectedContainerInner$.asObservable();
-  }
-
-  get downloadQueue$() {
-    return this.downloadQueueInner$.asObservable();
-  }
-
-  get deleteQueue$() {
-    return this.deleteQueueInner$.asObservable();
-  }
-
-  get uploadQueue$() {
-    return this.uploadQueueInner$
-      .asObservable()
-      .pipe(mergeMap(files => from(files)));
-  }
-
-  constructor(
-    private sasGenerator: SasGeneratorService,
-    private blobStorage: BlobStorageService,
-    private sanitizer: DomSanitizer
-  ) {}
+  constructor(private blobState: BlobStateService) {}
 
   onContainerClick(containerName: string): void {
-    this.selectedContainerInner$.next(containerName);
+    this.blobState.onContainerClick(containerName);
   }
 
   onDeleteItem(filename: string): void {
-    this.deleteQueueInner$.next(filename);
+    this.blobState.onDeleteItem(filename);
   }
 
   onDownloadItem(filename: string): void {
-    this.downloadQueueInner$.next(filename);
+    this.blobState.onDownloadItem(filename);
   }
 
   onFileSelected(files: FileList): void {
-    this.uploadQueueInner$.next(files);
-  }
-
-  private downloadFile = (filename: string) =>
-    this.getStorageOptionsWithContainer().pipe(
-      switchMap(options =>
-        this.blobStorage
-          .downloadBlobItem({
-            ...options,
-            filename
-          })
-          .pipe(
-            this.getDownloadUrlFromReponse(),
-            this.mapDownloadResponse(filename, options)
-          )
-      )
-    );
-
-  private deleteFile = (filename: string) =>
-    this.getStorageOptionsWithContainer().pipe(
-      switchMap(options =>
-        this.blobStorage
-          .deleteBlobItem({
-            ...options,
-            filename
-          })
-          .pipe(
-            this.mapDeleteResponse(filename, options),
-            this.finaliseBlobChange(options.containerName)
-          )
-      )
-    );
-
-  private uploadFile = (file: File) =>
-    this.getStorageOptionsWithContainer().pipe(
-      switchMap(options =>
-        this.blobStorage
-          .uploadToBlobStorage(file, {
-            ...options,
-            filename: file.name + new Date().getTime()
-          })
-          .pipe(
-            this.mapUploadResponse(file, options),
-            this.finaliseBlobChange(options.containerName)
-          )
-      )
-    );
-
-  private finaliseBlobChange = <T>(
-    containerName: string
-  ): MonoTypeOperatorFunction<T> => source =>
-    source.pipe(
-      finalize(
-        () =>
-          this.selectedContainerInner$.value === containerName &&
-          this.selectedContainerInner$.next(containerName)
-      )
-    );
-
-  private mapDownloadResponse = (
-    filename: string,
-    options: BlobContainerRequest
-  ): OperatorFunction<string, BlobItemDownload> => source =>
-    source.pipe(
-      map(url => ({
-        containerName: options.containerName,
-        filename,
-        url
-      })),
-      startWith({
-        containerName: options.containerName,
-        filename,
-        url: ''
-      })
-    );
-
-  private mapUploadResponse = (
-    file: File,
-    options: BlobContainerRequest
-  ): OperatorFunction<number, BlobItemUpload> => source =>
-    source.pipe(
-      map(progress => ({
-        filename: file.name,
-        containerName: options.containerName,
-        progress: parseInt(((progress / file.size) * 100).toString(), 10)
-      })),
-      startWith({
-        containerName: options.containerName,
-        filename: file.name,
-        progress: 0
-      })
-    );
-
-  private mapDeleteResponse = (
-    filename: string,
-    options: BlobContainerRequest
-  ): OperatorFunction<BlobDeleteResponse, BlobItem> => source =>
-    source.pipe(
-      map(() => ({
-        filename,
-        containerName: options.containerName
-      })),
-      startWith({
-        containerName: options.containerName,
-        filename
-      })
-    );
-
-  private getDownloadUrlFromReponse = (): OperatorFunction<
-    BlobDownloadResponseModel,
-    string
-  > => source =>
-    source.pipe(
-      switchMap(res =>
-        from(res.blobBody).pipe(
-          map(body => window.URL.createObjectURL(body)),
-          map(url => this.sanitizer.bypassSecurityTrustUrl(url) as string)
-        )
-      )
-    );
-
-  private scanEntries(): OperatorFunction<BlobItem, BlobItem[]> {
-    return source =>
-      source.pipe(
-        map(item => ({
-          [`${item.containerName}-${item.filename}`]: item
-        })),
-        scan<Dictionary<BlobItem>>(
-          (items, item) => ({
-            ...items,
-            ...item
-          }),
-          {}
-        ),
-        map(items => Object.values(items))
-      );
-  }
-
-  private getStorageOptions(): Observable<BlobStorageRequest> {
-    return this.sasGenerator.getSasToken();
-  }
-
-  private getStorageOptionsWithContainer(): Observable<BlobContainerRequest> {
-    return this.getStorageOptions().pipe(
-      withLatestFrom(this.selectedContainer$),
-      map(([options, containerName]) => ({ ...options, containerName }))
-    );
+    this.blobState.onFileSelected(files);
   }
 }
---
+++ src/app/azure-storage/services/blob-state.service.ts
@@ -0,0 +1,256 @@
+import { Injectable } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import {
+  BlobDeleteResponse,
+  BlobDownloadResponseModel
+} from '@azure/storage-blob';
+import {
+  BehaviorSubject,
+  from,
+  MonoTypeOperatorFunction,
+  Observable,
+  OperatorFunction,
+  Subject
+} from 'rxjs';
+import {
+  filter,
+  finalize,
+  map,
+  mergeMap,
+  scan,
+  startWith,
+  switchMap,
+  withLatestFrom
+} from 'rxjs/operators';
+import {
+  BlobContainerRequest,
+  BlobItem,
+  BlobItemDownload,
+  BlobItemUpload,
+  BlobStorageRequest,
+  Dictionary
+} from '../types/azure-storage';
+import { BlobStorageService } from './blob-storage.service';
+import { SasGeneratorService } from './sas-generator.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BlobStateService {
+  private selectedContainerInner$ = new BehaviorSubject<string>(undefined);
+  private uploadQueueInner$ = new Subject<FileList>();
+  private downloadQueueInner$ = new Subject<string>();
+  private deleteQueueInner$ = new Subject<string>();
+
+  containers$ = this.getStorageOptions().pipe(
+    switchMap(options => this.blobStorage.getContainers(options))
+  );
+  filesInContainer$ = this.selectedContainer$.pipe(
+    filter(containerName => !!containerName),
+    withLatestFrom(this.getStorageOptions()),
+    switchMap(([containerName, options]) =>
+      this.blobStorage.listBlobsInContainer({
+        ...options,
+        containerName
+      })
+    )
+  );
+  blobUploadProgress$ = this.uploadQueue$.pipe(
+    mergeMap(file => this.uploadFile(file)),
+    this.scanEntries()
+  );
+  blobDownloadResponse$ = this.downloadQueue$.pipe(
+    mergeMap(filename => this.downloadFile(filename)),
+    this.scanEntries()
+  );
+  blobDeleteResponse$ = this.deleteQueue$.pipe(
+    mergeMap(filename => this.deleteFile(filename)),
+    this.scanEntries()
+  );
+
+  get selectedContainer$() {
+    return this.selectedContainerInner$.asObservable();
+  }
+
+  get downloadQueue$() {
+    return this.downloadQueueInner$.asObservable();
+  }
+
+  get deleteQueue$() {
+    return this.deleteQueueInner$.asObservable();
+  }
+
+  get uploadQueue$() {
+    return this.uploadQueueInner$
+      .asObservable()
+      .pipe(mergeMap(files => from(files)));
+  }
+
+  constructor(
+    private sasGenerator: SasGeneratorService,
+    private blobStorage: BlobStorageService,
+    private sanitizer: DomSanitizer
+  ) {}
+
+  onContainerClick(containerName: string): void {
+    this.selectedContainerInner$.next(containerName);
+  }
+
+  onDeleteItem(filename: string): void {
+    this.deleteQueueInner$.next(filename);
+  }
+
+  onDownloadItem(filename: string): void {
+    this.downloadQueueInner$.next(filename);
+  }
+
+  onFileSelected(files: FileList): void {
+    this.uploadQueueInner$.next(files);
+  }
+
+  private downloadFile = (filename: string) =>
+    this.getStorageOptionsWithContainer().pipe(
+      switchMap(options =>
+        this.blobStorage
+          .downloadBlobItem({
+            ...options,
+            filename
+          })
+          .pipe(
+            this.getDownloadUrlFromReponse(),
+            this.mapDownloadResponse(filename, options)
+          )
+      )
+    );
+
+  private deleteFile = (filename: string) =>
+    this.getStorageOptionsWithContainer().pipe(
+      switchMap(options =>
+        this.blobStorage
+          .deleteBlobItem({
+            ...options,
+            filename
+          })
+          .pipe(
+            this.mapDeleteResponse(filename, options),
+            this.finaliseBlobChange(options.containerName)
+          )
+      )
+    );
+
+  private uploadFile = (file: File) =>
+    this.getStorageOptionsWithContainer().pipe(
+      switchMap(options =>
+        this.blobStorage
+          .uploadToBlobStorage(file, {
+            ...options,
+            filename: file.name + new Date().getTime()
+          })
+          .pipe(
+            this.mapUploadResponse(file, options),
+            this.finaliseBlobChange(options.containerName)
+          )
+      )
+    );
+
+  private finaliseBlobChange = <T>(
+    containerName: string
+  ): MonoTypeOperatorFunction<T> => source =>
+    source.pipe(
+      finalize(
+        () =>
+          this.selectedContainerInner$.value === containerName &&
+          this.selectedContainerInner$.next(containerName)
+      )
+    );
+
+  private mapDownloadResponse = (
+    filename: string,
+    options: BlobContainerRequest
+  ): OperatorFunction<string, BlobItemDownload> => source =>
+    source.pipe(
+      map(url => ({
+        filename,
+        containerName: options.containerName,
+        url
+      })),
+      startWith({
+        filename,
+        containerName: options.containerName,
+        url: ''
+      })
+    );
+
+  private mapUploadResponse = (
+    file: File,
+    options: BlobContainerRequest
+  ): OperatorFunction<number, BlobItemUpload> => source =>
+    source.pipe(
+      map(progress => ({
+        filename: file.name,
+        containerName: options.containerName,
+        progress: parseInt(((progress / file.size) * 100).toString(), 10)
+      })),
+      startWith({
+        filename: file.name,
+        containerName: options.containerName,
+        progress: 0
+      })
+    );
+
+  private mapDeleteResponse = (
+    filename: string,
+    options: BlobContainerRequest
+  ): OperatorFunction<BlobDeleteResponse, BlobItem> => source =>
+    source.pipe(
+      map(() => ({
+        filename,
+        containerName: options.containerName
+      })),
+      startWith({
+        filename,
+        containerName: options.containerName
+      })
+    );
+
+  private getDownloadUrlFromReponse = (): OperatorFunction<
+    BlobDownloadResponseModel,
+    string
+  > => source =>
+    source.pipe(
+      switchMap(res =>
+        from(res.blobBody).pipe(
+          map(body => window.URL.createObjectURL(body)),
+          map(url => this.sanitizer.bypassSecurityTrustUrl(url) as string)
+        )
+      )
+    );
+
+  private scanEntries<T extends BlobItem>(): OperatorFunction<T, T[]> {
+    return source =>
+      source.pipe(
+        map(item => ({
+          [`${item.containerName}-${item.filename}`]: item
+        })),
+        scan<Dictionary<T>>(
+          (items, item) => ({
+            ...items,
+            ...item
+          }),
+          {}
+        ),
+        map(items => Object.values(items))
+      );
+  }
+
+  private getStorageOptions(): Observable<BlobStorageRequest> {
+    return this.sasGenerator.getSasToken();
+  }
+
+  private getStorageOptionsWithContainer(): Observable<BlobContainerRequest> {
+    return this.getStorageOptions().pipe(
+      withLatestFrom(this.selectedContainer$),
+      map(([options, containerName]) => ({ ...options, containerName }))
+    );
+  }
+}
