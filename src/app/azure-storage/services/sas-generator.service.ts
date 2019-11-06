bca8913b6f5a1ef40ed102c679d7bc39053c226b
Stuart Tottle <stuarttottle@hotmail.com>
2019-11-06 11:17:49 +0000

    tidy up

--- src/app/azure-storage/services/sas-generator.service.ts
+++ src/app/azure-storage/services/sas-generator.service.ts
@@ -18,8 +18,7 @@ export class SasGeneratorService {
     }
     return of({
       storageUri: 'stottleblobstorage',
-      storageAccessToken:
-        'sv=2019-02-02&ss=b&srt=sco&sp=rwdlac&se=2019-11-14T19:20:59Z&st=2019-11-01T11:20:59Z&spr=https&sig=dG1zvo67VNDrqeWlrcrlz78W2ONVbYn2FlGJeFDifGg%3D'
+      storageAccessToken: environment.devSasToken
     });
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,20 +1,14 @@
 import { sasToken } from './environment.hidden';
 
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
+/* INFO
+'./environment.hidden' is git ignored and and looks like
+export const sasToken = '<SAStoken>';
+
+You will need to create the file and generate and include your own SAS token
+*/
 
 export const environment = {
   production: false,
   sasGeneratorUrl: 'https://localhost:5001',
   devSasToken: sasToken
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
