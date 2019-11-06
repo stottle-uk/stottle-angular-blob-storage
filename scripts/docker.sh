5fe6f7c9aa06f61f744e6c15f8fa0aabcd27e504
Stuart Tottle <stuarttottle@hotmail.com>
2019-11-06 10:10:09 +0000

    updatd docker script

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,7 @@
-docker build -t stottle/angular-blob-storage  .
+docker build -t stottleuk/angular-blob-storage  .
 
-docker tag stottle/angular-blob-storage stottlecontainerregistry.azurecr.io/angular-blob-storage
+docker tag stottleuk/angular-blob-storage stottlecontainerregistry.azurecr.io/angular-blob-storage
 
-docker run -p 4200:80 --rm stottlecontainerregistry.azurecr.io/angular-blob-storage
+docker run -p 4201:80 --rm stottlecontainerregistry.azurecr.io/angular-blob-storage
 
 # docker push stottlecontainerregistry.azurecr.io/angular-blob-storage
